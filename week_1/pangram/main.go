362500fb7b49e08a842e6fb86a2a88ffeb743095
kramar <kramarenko.konstantin@gmail.com>
2022-10-13 23:52:01 +0300

    implement pangram function

--- week_1/pangram/main.go
+++ week_1/pangram/main.go
@@ -1,10 +1,17 @@
 package pangram
 
+import "strings"
+
 // IsPangram checks if given string is a pangram
 func IsPangram(s string) bool {
-	// Тут має бути рішення
-	// написавши код - необхідно запустити тести
-	// Ці коментарі можна видаляти
-	// !ВАЖЛИВО - не забудьте виправити return
-	return false
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	counter := 0
+
+	for _, char := range alphabet {
+		if strings.Index(strings.ToLower(s), string(char)) != -1 {
+			counter++
+		}
+	}
+
+	return counter == len(alphabet)
 }
