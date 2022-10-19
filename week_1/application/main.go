d9460b2744e2dee768b196699ba864d52c3de664
kramar <kramarenko.konstantin@gmail.com>
2022-10-19 13:59:26 +0300

    make modules avaalable for import as a third party modules

--- week_1/application/main.go
+++ week_1/application/main.go
@@ -1,6 +1,10 @@
 package main
 
-import twelweDays "github.com/kramar/go-lessons/week_1/twelve-days"
+import (
+	"fmt"
+	twelweDays "github.com/kramar/go-lessons/week_1/twelve-days"
+)
 
 func main() {
+	fmt.Println(twelweDays)
 }
