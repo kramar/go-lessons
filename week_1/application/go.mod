c5bf1a7a6cf1b6b64b34bec2090f5d4b263c8d10
kramar <kramarenko.konstantin@gmail.com>
2022-10-19 14:05:44 +0300

    use imported modules

--- week_1/application/go.mod
+++ week_1/application/go.mod
@@ -1,3 +1,9 @@
 module main
 
 go 1.19
+
+require (
+	github.com/kramar/go-lessons/week_1/leap v0.0.0-20221019105926-d9460b2744e2
+	github.com/kramar/go-lessons/week_1/pangram v0.0.0-20221019105926-d9460b2744e2
+	github.com/kramar/go-lessons/week_1/twelve-days v0.0.0-20221019103412-27439165a443
+)
--- week_1/application/main.go
+++ week_1/application/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	twelweDays "github.com/kramar/go-lessons/week_1/twelve-days"
+	"github.com/kramar/go-lessons/week_1/leap"
+	"github.com/kramar/go-lessons/week_1/pangram"
 )
 
 func main() {
-	fmt.Println(twelweDays)
+	fmt.Println(twelweDays.Song())
+	fmt.Println(leap.IsLeapYear(2000))
+	fmt.Println(pangram.IsPangram("This is test string"))
 }
